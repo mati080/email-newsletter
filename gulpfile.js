0a37a9a6e5581701d14522f7e75afa51745642d0
Mateusz Gorzelak <mateuszgorzelak@carfax.com>
2021-02-23 18:48:47 +0100

    gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const inlineCss = require('gulp-inline-css');
 
 gulp.task('styles', () => {
     return gulp.src('./style/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('index.css'))
         .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('inline-css', function() {
+    return gulp.src('./public/*.html')
+        .pipe(inlineCss())
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('watch', () => {
